a249c7e08e96ee07eead33f5515dfb2c9145a80e
edoco89 <edo1cohen@gmail.com>
2018-10-23 11:32:19 +0300

    First commit

---
+++ js/util-service.js
@@ -0,0 +1,31 @@
+function makeId() {
+    var length = 6;
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return txt;
+}
+
+function getFromStorage(key) {
+    var val = localStorage.getItem(key);
+    return JSON.parse(val)
+}
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function getTimeStemp() {
+    return new Date().getTime();
+}
+
+// return a random number
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
