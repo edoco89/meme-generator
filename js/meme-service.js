7d4d8eb6b3aefbc98dd781d4af6d9da4d151785c
edoco89 <edo1cohen@gmail.com>
2018-10-23 12:26:19 +0300

    canvas added and basic js functions

---
+++ js/meme-service.js
@@ -0,0 +1,27 @@
+var gColor;
+var gPrevX = 0;
+var gPrevY = 0;
+var gPrevTimeStemp;
+
+
+function setInitialCoords(x, y) {
+    gPrevX = x;
+    gPrevY = y;
+}
+
+function setLastTimestamp(timeStamp) {
+    gPrevTimeStemp = timeStamp;
+}
+
+function setInitialCoords(x, y) {
+    gPrevX = x;
+    gPrevY = y;
+}
+
+
+function setColor(color) {
+    gColor = color;
+}
+
+
+
