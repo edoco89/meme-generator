28a8b0b385b5f4a48b672be82c7dfe56f6b33bc0
tom manger <tom.manger10@gmail.com>
2018-10-23 16:49:05 +0300

    upload image

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ var gCurrentTxtPos = 1;
 
 function init() {
     gCanvas = document.querySelector('#canvas');
-    gCanvas.width = document.body.clientWidth;
-    gCanvas.height = document.body.clientHeight;
+    // gCanvas.width = document.body.clientWidth;
+    // gCanvas.height = document.body.clientHeight;
     gCtx = canvas.getContext('2d');
     renderGallery();
 }
@@ -18,8 +18,13 @@ function handleCanvasClick(ev) {
 
 function onTextAdd(txt, ev) {
     gCtx.beginPath();
-    gCtx.font = "30px Arial";
-    gCtx.fillText(txt , 10, gCurrentTxtPos * 50);
+    gCtx.fillStyle = getColor();
+    gCtx.font = "50px Arial";
+    gCtx.fillText(txt, 10, gCurrentTxtPos * 50);
+
+    gCtx.strokeStyle = 'black';
+    gCtx.strokeText(txt, 10, gCurrentTxtPos * 50);
+
     gCurrentTxtPos++;
     var txtInput = document.getElementById('txt-add');
     txtInput.value = '';
@@ -36,9 +41,12 @@ function clearCanvas() {
 }
 
 function saveCanvas(elLink) {
-    elLink.href = canvas.toDataURL();
+    var imgContent = canvas.toDataURL();
+    console.dir(elLink);
+    elLink.href = imgContent;
     elLink.download = 'my-canvas.jpg';
 }
+
 
 
 //button hamburger toggle
@@ -47,15 +55,16 @@ function toggleMenu() {
 }
 
 
-function renderGallery(){
+function renderGallery() {
     var imgs = getGallery();
     var strHTML = imgs.map(img => {
         return `<img onclick="onImgClick('${img.url}')" class="img-gallery" src="${img.url}">`
-    },[]);
-    document.querySelector('.gallery-container').innerHTML = strHTML.join('');
+    }, []);
+    document.querySelector('.gallery').innerHTML = strHTML.join('');
 }
 
-function onImgClick(urlImg){
+function onImgClick(urlImg) {
+    imageToCanvas(urlImg);
     document.querySelector('.gallery-container').classList.add('hide');
     document.querySelector('.editor-container').classList.remove('hide');
 }
@@ -63,7 +72,27 @@ function onImgClick(urlImg){
 function imageToCanvas(imgUrl) {
     var img = new Image();
     img.src = imgUrl;
-    img.onload = function() {
-        gCtx.drawImage(img, 0, 0 ,canvas.width, canvas.height)
+    img.onload = function () {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        gCtx.drawImage(img, 0, 0, canvas.width, canvas.height)
     }
+}
+
+function onGalleryClick() {
+    document.querySelector('.gallery-container').classList.toggle('hide');
+    document.querySelector('.btn-top').innerText = 'Gallery';
+    document.querySelector('.editor-container').classList.toggle('hide');
+}
+
+// upload Image
+function onFileInputChange(ev) {
+    handleImageFromInput(ev, renderCanvas);
+}
+
+function renderCanvas(img) {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    gCtx.drawImage(img, 0, 0);
+    onGalleryClick();
 }
